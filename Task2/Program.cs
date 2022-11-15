82da4b2ec3dc88a9b564f5d8ea7d4d4d89b0299a
Vadim Sh <2423423@mail.ru>
2022-11-15 15:24:01 +0300

    complite+ task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,9 +4,12 @@ Console.WriteLine("Введите первое число");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numB = Convert.ToInt32(Console.ReadLine());
-if (numA > numB ) {
+if ( numA > numB ) {
 Console.WriteLine($"max = {numA} min = {numB}");
 }
-else {
+else if ( numA < numB ){
 Console.WriteLine($"max = {numB} min = {numA}");
+}
+else {
+Console.WriteLine("Введены одинаковые числа");
 }
