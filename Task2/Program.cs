f68a803e138ac2976e608325a9522f225ac9ef7a
Vadim Sh <2423423@mail.ru>
2022-11-15 15:19:06 +0300

    complite task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,8 +5,8 @@ int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numB = Convert.ToInt32(Console.ReadLine());
 if (numA > numB ) {
-Console.WriteLine($"Число {numA} больше, чем {numB}");
+Console.WriteLine($"max = {numA} min = {numB}");
 }
 else {
-Console.WriteLine($"Число {numB} больше, чем {numA}");
+Console.WriteLine($"max = {numB} min = {numA}");
 }
