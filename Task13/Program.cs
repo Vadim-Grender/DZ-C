8ee5c5525ba0e48188a27757811e6cf4d4c05994
Vadim Sh <2423423@mail.ru>
2022-11-19 19:35:15 +0300

    add and complite task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string s = Convert.ToString(Math.Abs(num));
+char[] digMas = new char[s.Length];
+for (int i = 0; i < s.Length; i++)
+{
+    digMas[i] = s[i];
+}
+if (s.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + digMas[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
