845dd27c8c11d4903d80815f85fba27a9713edc3
Vadim Sh <2423423@mail.ru>
2022-11-25 19:15:20 +0300

    add and complite task29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Write("Введите кол-во элементов: ");
+string? count = Console.ReadLine();
+int[] array = new int[0];
+if (int.TryParse(count, out int tryCount) & tryCount > 0)
+{
+    array = new int[tryCount];
+    for (int i = 0; i < tryCount; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        string? digit = Console.ReadLine();
+        if (int.TryParse(digit, out int tryDigit))
+        {
+        array[i] = tryDigit;
+        }
+        else
+        {
+            Console.WriteLine("Введено нечисловое значение, значение  = 0"); 
+        }
+    }
+    Console.Write('[' + string.Join(", ", array) + ']');
+}
+else
+{
+    Console.WriteLine("Введено недопустимое значение");
+}
