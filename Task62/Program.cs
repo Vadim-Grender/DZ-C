07f072de5ae0fc6b5f83a3ff4542e6a68962e377
Vadim Sh <2423423@mail.ru>
2022-12-11 18:30:50 +0300

    add and complite task62

---
+++ Task62/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,3}", matrix[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите кол-во строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+int row = 0;
+int col = 0;
+int dx = 1;
+int dy = 0;
+int dirChanges = 0;
+int visits = m;
+
+for (int i = 0; i < matrix.Length; i++)
+{
+    matrix[row, col] = i + 1;
+    if (--visits == 0)
+    {
+        visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+        int temp = dx;
+        dx = -dy;
+        dy = temp;
+        dirChanges++;
+    }
+
+    col += dx;
+    row += dy;
+}
+
+PrintMatrix(matrix);
